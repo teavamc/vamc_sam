4c46d7f7caf7780ab27d54e6531ebd8a46df0286
teavamc <teavamc@163.com>
2020-09-21 20:01:23 +0800

    feat:解决多模块打包问题,输出到 deploy 模组等

--- vamc-sam-start/src/main/java/com/teavamc/vamcsam/VamcSamApplication.java
+++ vamc-sam-start/src/main/java/com/teavamc/vamcsam/VamcSamApplication.java
@@ -1,13 +1,16 @@
 package com.teavamc.vamcsam;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class VamcSamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VamcSamApplication.class, args);
+        log.info("Every thing will perfect!");
     }
 
 }
