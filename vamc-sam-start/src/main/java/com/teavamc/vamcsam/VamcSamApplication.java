d0491440127ef23a5969ae3f958733b5c5e008d0
teavamc <teavamc@163.com>
2020-09-25 18:03:34 +0800

    fix:完成 springboot 的 nacos 的配置

--- vamc-sam-start/src/main/java/com/teavamc/vamcsam/VamcSamApplication.java
+++ vamc-sam-start/src/main/java/com/teavamc/vamcsam/VamcSamApplication.java
@@ -1,6 +1,7 @@
 package com.teavamc.vamcsam;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import com.teavamc.vamsam.dubboutils.EnableDubboUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
@@ -8,8 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-@SpringBootApplication
 @EnableDubbo(scanBasePackages = {"com.teavamc.vamcsam.service.dubbo"})
+@SpringBootApplication
+@NacosPropertySource(dataId = "vamc-sam", autoRefreshed = true)
 @MapperScan("com.teavamc.vamcsam.dao")
 @EnableDubboUtil
 public class VamcSamApplication {
