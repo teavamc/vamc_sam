48b1e7b53dfc415cd78be5d763d7c2c02c73104a
teavamc <teavamc@163.com>
2020-10-20 16:15:08 +0800

    fix:学习笔记..

---
+++ vamc-sam-service/src/main/java/com/teavamc/vamcsam/service/practice/GraphTest.java
@@ -0,0 +1,12 @@
+package com.teavamc.vamcsam.service.practice;
+
+import com.google.common.graph.Graph;
+
+/**
+ * @author 墨诚 mocheng@byai.com
+ * @Package com.teavamc.vamcsam.service.practice
+ * @date 2020/10/20 14:34
+ */
+public class GraphTest {
+
+}
