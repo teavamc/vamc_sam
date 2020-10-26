5cfcbcc3baece5f448ff0d0e1e698bf42f7910a1
teavamc <teavamc@163.com>
2020-10-26 11:31:21 +0800

    fix:学习笔记..java spi demo

---
+++ vamc-sam-service/src/main/java/com/teavamc/vamcsam/service/practice/spi/Robot.java
@@ -0,0 +1,10 @@
+package com.teavamc.vamcsam.service.practice.spi;
+
+/**
+ * @author 墨诚 mocheng@byai.com
+ * @Package com.teavamc.vamcsam.service.practice.spi
+ * @date 2020/10/26 10:22
+ */
+public interface Robot {
+    void sayHello();
+}
