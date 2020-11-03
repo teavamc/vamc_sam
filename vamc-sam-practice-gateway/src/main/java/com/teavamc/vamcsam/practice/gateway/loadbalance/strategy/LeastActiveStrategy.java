15d1c561ec108c95239b0d40c46aab6a72cdd9e1
teavamc <teavamc@163.com>
2020-11-03 20:53:35 +0800

    feat:瞎折腾学习--网关的负载均衡算法差两个

--- vamc-sam-practice-gateway/src/main/java/com/teavamc/vamcsam/practice/gateway/loadbalance/strategy/LeastActiveStrategy.java
+++ vamc-sam-practice-gateway/src/main/java/com/teavamc/vamcsam/practice/gateway/loadbalance/strategy/LeastActiveStrategy.java
@@ -4,6 +4,7 @@ import com.teavamc.vamcsam.practice.gateway.domain.ServerProcessDTO;
 import com.teavamc.vamcsam.practice.gateway.loadbalance.ILoadBalance;
 
 /**
+ * //todo 最小化活跃 或者 最小负载
  * @author 墨诚 mocheng@byai.com
  * @Package com.teavamc.vamcsam.practice.gateway.loadbalance.strategy
  * @date 2020/11/2 10:31
